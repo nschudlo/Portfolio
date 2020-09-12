8361af4ec123a2a22d4992cd4d7e800d1b1d154a
Nick Schudlo <nschudlo@gmail.com>
2020-09-11 23:20:14 -0400

    Style the game menu something

--- web-react/src/components/PortfolioView.js
+++ web-react/src/components/PortfolioView.js
@@ -8,7 +8,9 @@ import * as GameConstants from "../GameConstants";
 export default function PortfolioView(props) {
 	return (
 		<div>
-			<GameList list={GameConstants.GAME_LIST}/>
+			<div className="game-menu">
+				<GameList list={GameConstants.GAME_LIST}/>
+			</div>
 			<GamesView list={GameConstants.GAME_LIST}/>
 		</div>
 	);
